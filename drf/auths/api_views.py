2ed71e689115f206bfbc1eb2e8dd5e76f788d577
vlf0 <dr.dnb2000@gmail.com>
2023-12-08 04:00:17 +0700

    First full ViewSet implemented.

--- drf/auths/api_views.py
+++ drf/auths/api_views.py
@@ -1,22 +1,27 @@
 from django.contrib.auth import get_user_model
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Woman
 from .serializers import WomanSerializer
 
 CustomUser = get_user_model()
 
 
-class WomanDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+class WomanViewSet(viewsets.ModelViewSet):
     queryset = Woman.objects.all()
     serializer_class = WomanSerializer
 
 
-class WomanCreateAPIView(generics.CreateAPIView):
-    queryset = Woman.objects.all()
-    serializer_class = WomanSerializer
-
-
-class WomanListAPIView(generics.ListCreateAPIView):
-    queryset = Woman.objects.all()
-    serializer_class = WomanSerializer
+# class WomanDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Woman.objects.all()
+#     serializer_class = WomanSerializer
+#
+#
+# class WomanCreateAPIView(generics.CreateAPIView):
+#     queryset = Woman.objects.all()
+#     serializer_class = WomanSerializer
+#
+#
+# class WomanListAPIView(generics.ListCreateAPIView):
+#     queryset = Woman.objects.all()
+#     serializer_class = WomanSerializer
 
--- drf/auths/urls.py
+++ drf/auths/urls.py
@@ -1,14 +1,17 @@
-from django.urls import path
+from rest_framework import routers
+from django.urls import path, include
 from . import api_views
 from . import views
 
+w_router = routers.SimpleRouter()
+w_router.register(r'test', api_views.WomanViewSet)
 
 urlpatterns = [
     path('', views.IndexProfileView.as_view(), name='index'),
     path('registration/', views.UserRegistration.as_view(), name='registration'),
     path('out/', views.CustomLogout.as_view(), name='logout'),
     path('profile/', views.UserProfileView.as_view(), name='profile'),
-    path('api/v1/womenlist/', api_views.WomanListAPIView.as_view()),
-    path('api/v1/test/create/', api_views.WomanCreateAPIView.as_view()),
-    path('api/v1/test/<int:pk>/', api_views.WomanDetailAPIView.as_view()),
+    # path('api/v1/womenlist/', api_views.WomanListAPIView.as_view()),
+    path('api/v1/', include(w_router.urls)),
+
 ]
