85368bf7640264d8eee7a204c57673a7493d2ea3
vlf0 <dr.dnb2000@gmail.com>
2023-12-12 21:56:10 +0700

    First REACT component was created and stylising (login form).

---
+++ frontend/src/components/LoginForm.jsx
@@ -0,0 +1,11 @@
+export function TestComp() {
+    return (
+        <form class="login_form" action="">
+            <span>Login</span> <input type="text" name="username" />
+            <span>Password</span>  <input type="password" name="password" />
+            <input type="submit" value="Вход"></input>
+        </form>
+    )
+}
+
+
